b0d29355baba8ac46418abb28c851e52ab2207a2
THALLAPAKAMUNEENDRA <94665969+THALLAPAKAMUNEENDRA@users.noreply.github.com>
2023-04-13 17:23:08 +0530

    Myway Project

---
+++ Myways.js
@@ -0,0 +1,21 @@
+function fizzBuzzPhone(phoneNumber, name, email) {
+    let sum = 0;
+    for (let i = 0; i < phoneNumber.length; i++) {
+      sum += parseInt(phoneNumber[i]);
+    }
+    
+    for (let i = 1; i <= sum; i++) {
+      if (i % 4 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+      } else if (i % 4 === 0) {
+        console.log("Fizz");
+      } else if (i % 5 === 0) {
+        console.log("Buzz");
+      } else {
+        console.log(i);
+      }
+    }
+    
+    console.log("Name: " + name);
+    console.log("Email: " + email);
+  }
